90dacdf8e6902009603baed7c5443695b352be31
TalesPalma <tales.palma_2022@outlook.com>
2024-04-17 15:04:45 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import './components/Banner/Banner.js';
+import Banner from './components/Banner/Banner.js';
 
 function App() {
   return (
     <div className="App">
+      <Banner />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
